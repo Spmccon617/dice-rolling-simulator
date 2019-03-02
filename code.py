641c558683282ac2cc846abe474066da258537ae
Sean McConnell <47701446+Spmccon617@users.noreply.github.com>
2019-03-02 18:36:50 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,16 @@
+# February 27,2019
+# Trying to create a dice rolling simulator
+
 from random import randint
 
 while True:
 	number = randint(1,6)
 	print(number)
-	rollAgain = input("Do you want to roll again? (y/n)?")
-	
+	rollAgain = input("Do you want to roll again?")
+
+# Have to put quotations around yes to be able to run in terminal
 	if rollAgain is "yes":
 		continue
 	else:
-		print("quitting now")
+		print("quitting now...")
 		break
